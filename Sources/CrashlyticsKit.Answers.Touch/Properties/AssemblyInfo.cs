0e47e46d609ddf3e181e81c6398abb334e4224c9
Michel Moreira <michel@michelmoreira.eti.br>
2016-06-01 23:23:42 -0300

    Assembly for Answers with Crashlytics Only

---
+++ Sources/CrashlyticsKit.Answers.Touch/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Crashlytics.Answers.Touch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("michel")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
